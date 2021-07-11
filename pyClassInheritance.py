17286e3d5876f86697d5fe7cc48c1094595b2515
Robert Musungu Abisai <robertmusungu3@gmail.com>
2021-07-11 23:28:39 +0300

    Create pyClassInheritance.py
    Updated Inheritance py Code

---
+++ pyClassInheritance.py
@@ -0,0 +1,34 @@
+class Familymembers:
+    """This is class for family members within Ekavala Village"""
+    def __init__(self, name, status, age, family, gender):
+
+        self.fullname = name
+        self.merital = status
+        self.age = age
+        self.family = family
+        self.gender = gender
+
+    def intro(self):
+        print(f"My name is {self.fullname}. I'm {self.age} yrs, {self.merital} and happy. I'm a {self.gender} from {self.family} family")
+
+class Musungus(Familymembers):
+    def __init__(self, name, status, age, family, gender, course):
+        super().__init__(name, status, age, family,gender)
+        self.course = course
+    def intro(self):
+        print("My name is" + self.fullname + "from" + self.family + "family")
+
+
+fifthborn = Musungus("Esau Otenyo","married","47","Musungus","male","Agriculture")
+fifthborn.fullname
+print("\n***")
+fifthborn.intro()
+print("***\n")
+
+print(f"{fifthborn.gender}\n")
+print(f"{fifthborn.course}\n")
+
+firstborn = Familymembers("John Anyangu","married",67,"Musungus","Man")
+print("***\n")
+print(firstborn.intro())
+print("***\n")
